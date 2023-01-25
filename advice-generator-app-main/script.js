d924288bef279c3d0db4e182268b918641734142
Yasin VAR <yasin.var.yv@gmail.com>
2023-01-25 20:47:13 +0300

    script update

--- advice-generator-app-main/script.js
+++ advice-generator-app-main/script.js
@@ -1,27 +1,28 @@
-
 const button = document.getElementById('btn');
-const advice = document.getElementsByClassName('advice');
+const cards = document.getElementsByClassName('advice');
 
 button.addEventListener('click', function myFunction() {
-    let numbers = [];
-    for (let element of advice){
-        let slip_id = getRandomId();
+    let randomNumbers = Array.from({length: 3}, _ => Math.floor(Math.random()*175)+ 1);
+    let advices = [];
+    let slip_id = randomNumbers;
+    const urls = [`https://api.adviceslip.com/advice/${slip_id[0]}`, `https://api.adviceslip.com/advice/${slip_id[1]}`, `https://api.adviceslip.com/advice/${slip_id[2]}`];
+    
+    while (slip_id.length !== new Set(slip_id).size || slip_id.includes(67) || slip_id.includes(22) || slip_id.includes(48)) {
+    slip_id = randomNumbers
+    }
 
-        while (numbers.includes(slip_id)) {
-            slip_id = getRandomId();
-        }
-
-        numbers.push(slip_id);
-
-    fetch(`https://api.adviceslip.com/advice/${slip_id}`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-        element.innerHTML = data.slip.advice;
+    const requests = urls.map(url => fetch(url));
+    Promise.all(requests)
+        .then(responses => {
+            return Promise.all(responses.map(res => res.json()))
         })
-        .catch(error => console.error(error));
-}});
-
-function getRandomId() {
-    return (Math.floor(Math.random() * 175) + 1);
-}
+        .then(data => {
+            data.forEach(advice => {
+                advices.push(advice.slip.advice);
+                for (let i = 0; i < advices.length; i++) {
+                    cards[i].innerHTML = advices[i];
+                }
+            });
+        })
+        .catch(error => console.log(error));
+});
